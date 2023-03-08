629b2771d7079adec1fc5a5347496779cd88e91b
0027016965 <huang.chengyi2@iwhalecloud.com>
2023-03-08 11:01:23 +0800

    Initial commit

---
+++ src/main/java/org/fenixsoft/jvm/IndepthUnderstandingOfJavaVirtualMachineApplication.java
@@ -0,0 +1,17 @@
+package org.fenixsoft.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Decription
+ * @Author NEIL
+ * @Date 2023/3/8 10:48
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class IndepthUnderstandingOfJavaVirtualMachineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IndepthUnderstandingOfJavaVirtualMachineApplication.class, args);
+    }
+}
